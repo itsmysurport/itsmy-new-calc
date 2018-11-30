043f4f547a66e777042126f88ae126866d745428
itsmysurport <itsmysurport@gmail.com>
2018-12-01 00:27:02 +0900

    ADD operator Function

--- new_calc.py
+++ new_calc.py
@@ -1,6 +1,9 @@
 from new_calc_ui import *
 
 resultLabel = ''
+first = ''
+second = ''
+oper = ''
 
 def numberEvent(self):
     self.button1_0.clicked.connect(lambda x : self.addNumber('0'))
@@ -13,7 +16,23 @@ def numberEvent(self):
     self.button1_7.clicked.connect(lambda x : self.addNumber('7'))
     self.button1_8.clicked.connect(lambda x : self.addNumber('8'))
     self.button1_9.clicked.connect(lambda x : self.addNumber('9'))
-#    self.buttonPlus.clicked.connect()
+    self.buttonPlus.clicked.connect(lambda x : self.operator('+'))
+    self.resultButton.clicked.connect(self.resultFunc)
+
+def resultFunc(self):
+    global first, oper, second
+    if oper:
+        second = self.result.text()
+        self.resultText = eval(first + oper + second)
+        self.result.setText(str(self.resultText))
+
+def operator(self, a):
+    global first, oper
+    if not first:
+        first = self.result.text()
+        self.result.setText('0')
+        oper = a
+        print(first)
 
 def addNumber(self, a):
     global resultLabel
@@ -28,6 +47,8 @@ def addNumber(self, a):
 
 Ui_New_calc.numberEvent = numberEvent
 Ui_New_calc.addNumber = addNumber
+Ui_New_calc.operator = operator
+Ui_New_calc.resultFunc = resultFunc
 
 if __name__ == "__main__":
     import sys
